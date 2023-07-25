68d8e24d2323e685477187d51018d9fa926fb6a6
SH-indeyong <ohsh1115@gmail.com>
2023-07-25 21:03:07 +0900

    URL Reverse

--- instagram1/urls.py
+++ instagram1/urls.py
@@ -11,7 +11,8 @@ register_converter(DayConverter, 'day')
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:pk>/', views.post_detail),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+
     # re_path(r'(?P<pk>\d+)/$', views.post_detail)
     # path('archives/<int:year>/', views.archives_year),
     # re_path(r'archives/(?P<year>\d{4})/', views.archives_year),
@@ -21,4 +22,4 @@ urlpatterns = [
     path('archive/<year:year>/', views.post_archive_year, name='post_archive_year'),
     # path('archive/<year:year>/<month:month>/', views.post_archive_month, name='post_archive_month'),
     # path('archive/<year:year>/<month:month>/<day:day>/', views.post_archive_day, name='post_archive_day')
-]
+] 

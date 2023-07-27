350ad326aa35b3d9fdc12d5461320a80aa8e4f49
SH-indeyong <ohsh1115@gmail.com>
2023-07-27 17:38:42 +0900

    signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,26 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from .forms import ProfileForm
 from .models import Profile
 
+User = get_user_model
+
+
 # Create your views here.
 class ProfileView(LoginRequiredMixin, TemplateView):
-    template_name = 'accounts/profile.html'
+    template_name = "accounts/profile.html"
+
 
 profile = ProfileView.as_view()
+
 
 @login_required
 def profile_edit(request):
@@ -18,22 +28,40 @@ def profile_edit(request):
         profile = request.user.profile
     except Profile.DoesNotExist:
         profile = None
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             profile = form.save(commit=False)
             profile.user = request.user
             profile.save()
-            return redirect('profile')
+            return redirect("profile")
     else:
         form = ProfileForm(instance=profile)
-    return render(request, 'accounts/profile_form.html', {
-        'form': form,
-    })
+    return render(
+        request,
+        "accounts/profile_form.html",
+        {
+            "form": form,
+        },
+    )
 
-def signup(request):
-    pass
+
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    template_name = "accounts/signup_form.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+        auth_login(self.request, user)
+        return response
+
+
+signup = SignupView.as_view()
+
 
 def logout(request):
     pass
